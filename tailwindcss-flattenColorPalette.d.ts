4800617069f9feaa064fc920bcde03431b7d4945
Aryan <aryan@Aryans-MacBook-Air.local>
2024-10-12 16:34:51 +0530

    Initial commit

---
+++ tailwindcss-flattenColorPalette.d.ts
@@ -0,0 +1,6 @@
+declare module 'tailwindcss/lib/util/flattenColorPalette' {
+    // You can specify the actual types if you know them, or use 'any' as a placeholder
+    const flattenColorPalette: any;
+    export default flattenColorPalette;
+  }
+  
